jpeeler/operator-lifecycle-manager

fix(olm): handle CSV install failure gracefully
Specifically, don't continuously retry CSV installation when a forbidden
error is returned. A new CSV phase has been created for this scenario,
but has been labeled as "Failed" in the spirit of not adding additional
complexity to the possible phase states to keep up with from the user
perspective.

pkg/controller/operators/olm/operator.go
@@ -1119,6 +1119,11 @@ func (a *Operator) transitionCSVState(in v1alpha1.ClusterServiceVersion) (out *v
 		"phase":     in.Status.Phase,
 	})
 
+	if in.Status.Phase == v1alpha1.CSVPhaseFailedNoRetry {
+		// seriously, don't retry. will change phase in the event of an intentional requeue
+		return
+	}
+
 	out = in.DeepCopy()
 	now := a.now()
 
@@ -1308,6 +1313,11 @@ func (a *Operator) transitionCSVState(in v1alpha1.ClusterServiceVersion) (out *v
 		}
 
 		if syncError = installer.Install(strategy); syncError != nil {
+			if k8serrors.IsForbidden(syncError) {
+				logger.Infof("Setting CSV to failed without retry due to forbidden error: %v", syncError)
+				out.SetPhaseWithEvent(v1alpha1.CSVPhaseFailed, v1alpha1.CSVReasonComponentFailedNoRetry, fmt.Sprintf("install strategy failed: %s", syncError), now, a.recorder)
+				return
+			}
 			out.SetPhaseWithEvent(v1alpha1.CSVPhaseFailed, v1alpha1.CSVReasonComponentFailed, fmt.Sprintf("install strategy failed: %s", syncError), now, a.recorder)
 			return
 		}

kevinrizza/operator-framework-olm

error is returned. A new deployment error and CSV reason has been created
for this scenario. The CSV sync loop will not process a CSV that has the
no retry reason set.
Upstream-repository: operator-lifecycle-manager
Upstream-commit: 21655904f5b3dea477901358dd6aad26625e8ef2

staging/operator-lifecycle-manager/pkg/controller/operators/olm/operator.go
@@ -1119,6 +1119,12 @@ func (a *Operator) transitionCSVState(in v1alpha1.ClusterServiceVersion) (out *v
 		"phase":     in.Status.Phase,
 	})
 
+	if in.Status.Reason == v1alpha1.CSVReasonComponentFailedNoRetry {
+		// will change phase out of failed-no-retry in the event of an intentional requeue
+		logger.Debugf("skipping sync for CSV in failed-no-retry state")
+		return
+	}
+
 	out = in.DeepCopy()
 	now := a.now()
 
@@ -1308,6 +1314,11 @@ func (a *Operator) transitionCSVState(in v1alpha1.ClusterServiceVersion) (out *v
 		}
 
 		if syncError = installer.Install(strategy); syncError != nil {
+			if install.IsErrorUnrecoverable(syncError) {
+				logger.Infof("Setting CSV reason to failed without retry: %v", syncError)
+				out.SetPhaseWithEvent(v1alpha1.CSVPhaseFailed, v1alpha1.CSVReasonComponentFailedNoRetry, fmt.Sprintf("install strategy failed: %s", syncError), now, a.recorder)
+				return
+			}
 			out.SetPhaseWithEvent(v1alpha1.CSVPhaseFailed, v1alpha1.CSVReasonComponentFailed, fmt.Sprintf("install strategy failed: %s", syncError), now, a.recorder)
 			return
 		}